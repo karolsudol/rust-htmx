be20ca94807ac136ec9f4d9d1842f746245e75dd
karolsudol <karol@sudoltrust.com>
2024-05-28 17:51:33 +0100

    params query

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use axum::{routing::get, Router};
+use axum::{extract::Query, response::Html, routing::get, Router};
+use rand::{thread_rng, Rng};
+use serde::Deserialize;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let app = Router::new().route("/", get(handler));
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+#[derive(Deserialize)]
+struct RangeParameters {
+    start: usize,
+    end: usize,
+}
+
+async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
+    let random_number = thread_rng().gen_range(range.start..range.end);
+    Html(format!("<h1>Random Number: {}</h1>", random_number))
 }
