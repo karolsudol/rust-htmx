0e249a2671327fc7748865828883c82c218b61ae
karolsudol <karol@sudoltrust.com>
2024-07-01 15:59:30 +0100

    axum server fixed

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,11 @@
 #[tokio::main]
 async fn main() {
     env_logger::init();
-
+    
     let app = Router::new()
-        .route("/", get(index))
-    axum::Server::bind(&"0.0.0.0:42069".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app.into_make_service()).await.unwrap();
+
 }
